137530be00cd78fcb04a002323f53d058ac01cae
PhilKMcC <pkmccaffrey@loyola.edu>
2026-06-19 20:11:23 -0500

    tried to add something to help with distances

---
+++ Assets/Scripts/ScaleWithDistance.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ScaleWithDistance : MonoBehaviour
+{
+    public Vector3 baseScale = Vector3.one;
+    public static float scalingIntensity = (1/10f); //for every y unit vertically, scale by this much 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        baseScale = gameObject.transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = baseScale * calcScale(); ;
+    }
+
+    public float calcScale()
+    {
+        return 1 + scalingIntensity * -1 * transform.position.y;
+    }
+}
