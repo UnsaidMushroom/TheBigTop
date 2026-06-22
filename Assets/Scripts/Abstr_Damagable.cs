126c29fa280f478fc02928ad20887f69a6cebaf9
UnsaidMushroom <ultimategamerst@gmail.com>
2026-06-21 20:33:28 -0400

    Basic Projectile

---
+++ Assets/Scripts/Abstr_Damagable.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using System.Collections.Generic;
+public abstract class Abstr_Damagable : MonoBehaviour
+{
+
+
+    public static HashSet<string> friendliesTags;
+    public static HashSet<string> enemiesTags;
+
+    protected string myTag = null;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        myTag = gameObject.tag;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public abstract void Damage();
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (myTag == null) { myTag = gameObject.tag; }
+
+        string otherTag = collision.gameObject.tag;
+        Debug.Log("collision! me: " + myTag + "; other: " + otherTag);
+
+        if (!friendliesTags.Contains(myTag) && friendliesTags.Contains(otherTag))
+        {
+            Damage();
+        }
+        if (!enemiesTags.Contains(myTag) && enemiesTags.Contains(otherTag))
+        {
+            Damage();
+        }
+    }
+
+    public static void initializeSets()
+    {
+        friendliesTags = new HashSet<string>();
+        friendliesTags.Add("Friendly");
+
+        enemiesTags = new HashSet<string>();
+        enemiesTags.Add("Enemy");
+
+
+    }
+}
+
---
+++ Assets/Scripts/ProjectileBasic.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class ProjectileBasic : Abstr_Damagable
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Rigidbody2D myBody;
+    public float speed = 15f;
+    private float timer = 2;
+    void Start()
+    {
+        if (myBody == null) { myBody = gameObject.GetComponent<Rigidbody2D>(); }
+        myBody.SetRotation(0);
+        myBody.linearVelocity = transform.up * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            Damage();
+        }
+    }
+
+    public override void Damage()
+    {
+        Destroy(gameObject);
+    }
+}
