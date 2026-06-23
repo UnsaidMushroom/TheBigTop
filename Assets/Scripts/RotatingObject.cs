6f433de5bb0abfd1cf3abc7e3daa0fb45206d87d
UnsaidMushroom <ultimategamerst@gmail.com>
2026-06-23 14:24:08 -0400

    im missing something

--- Assets/Scripts/RotatingObject.cs
+++ Assets/Scripts/RotatingObject.cs
@@ -1,4 +1,6 @@
+using System.Drawing;
 using System.Net.Sockets;
+using Unity.VisualScripting;
 using UnityEditor.Experimental;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -10,41 +12,27 @@ public class RotatingObject : MonoBehaviour
 
     public float rotateSpeed = 2f;
 
-    public Vector3 centerPosition;
-    public Vector3 rightPosition;
-    public Vector3 leftPosition;
+    public Vector3 centerPos;
+    private Vector3 destPos;
 
-    private bool moving;
+    private float yRad;
+    private float xRad;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        centerPosition.x = -1;
-        centerPosition.y = -2.85f;
-        rightPosition.x = 3.5f;
-        rightPosition.y = -4.25f;
-        leftPosition.x = -5.5f;
-        leftPosition.y = -4.25f;
-        moving = false;
+        centerPos.x = -1;
+        centerPos.y = -5;
+        xRad = transform.position.x - centerPos.x;
+        yRad = transform.position.y - centerPos.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //on right press, left to center, center to right, right to left
-        //if right button was pressed this frame
-            //if currently left position, set dest to center
-            //if currently center position, set dest to right
-            //if currently right position, set dest to left AND remove hitbox rule
-            //set moving to true
-        //on left press, right to center, center to left, left to right
-            //if right button was pressed this frame
-            //if currently left position, set dest to center
-            //if currently center position, set dest to right
-            //if currently right position, set dest to left AND remove hitbox rule
-            //set moving to true
-        
-        transform.position = Vector2.MoveTowards(this.transform.position, rightPosition, rotateSpeed * Time.deltaTime);
+        destPos.y = Mathf.Sin(Time.deltaTime) * xRad + centerPos.y;
+        destPos.x = Mathf.Cos(Time.deltaTime) * yRad + centerPos.x;
+        transform.position = Vector2.MoveTowards(this.transform.position, destPos, rotateSpeed * Time.deltaTime);
     }
 
-    //Hi, for the rotation i was imagining several degrees of rotation, such that you can swivel in live time and react to attacks, not switching between a few set locations. something probably involving sin/cos, at least in my vision
 }
