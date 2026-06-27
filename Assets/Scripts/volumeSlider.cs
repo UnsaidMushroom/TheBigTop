2e5bda0fdeeb35a9f0b731263308ccd53a25be96
UnsaidMushroom <ultimategamerst@gmail.com>
2026-06-26 22:50:18 -0400

    Ported Volume Slidier

---
+++ Assets/Scripts/volumeSlider.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class volumeSlider : MonoBehaviour
+{
+
+    public Slider volumeSlide;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("Volume"))
+        {
+            volumeSlide.value = PlayerPrefs.GetFloat("Volume");
+        }
+        else
+        {
+            PlayerPrefs.SetFloat("Volume", 1);
+            volumeSlide.value = PlayerPrefs.GetFloat("Volume");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void changeVol()
+    {
+        AudioListener.volume = volumeSlide.value;
+        PlayerPrefs.SetFloat("Volume", volumeSlide.value);
+    }
+}
